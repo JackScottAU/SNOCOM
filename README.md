f85ac58c61708a49e7ab1bfc2a6c80556e03b0d5
Jack Scott <jack@jackscott.id.au>
2025-01-15 14:20:03 +1100

    A few more instructions and notes.

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # SNOCOM Emulator
 
-Designed to emulate the SNOCOM computer: https://www.pearcey.org.au/blog/2020/snocom-and-cirrus/
-
-https://ses.library.usyd.edu.au/handle/2123/16005
+Designed to emulate the SNOCOM computer built in 1960 by David Wong from Sydney University for the Snowy Hydro.
 
 8KiB (2048 32-bit words)
 
 Plus accumulator, counter, and instruction pointer.
+
+## Differences between the emulator and the prototype.
+
+Output = in ASCII, only one output device.
+
+Also has the ability to load the machine's memory with a rom file, to avoid having to load via 'paper tape'.
+
+The conditional transfer doesn't use the Z physical switch (because there isn't one), it asks via stdin what to do.
 
 ## Intended Usage
 
@@ -16,19 +22,39 @@ snocom -b biosfile.rom < intape.txt > outtype.txt -d 2048
 
 -i is the input tape file
 
--o is the output typewriter file
+-o is the output tape file
 
 -d dumps the contents of memory after the specified number of clock cycles (to aid with debugging)
 
 registerfile.2048
 memorydump.2048
 
+typewriter goes to stdout
+
+z switch is on stdin
+
 biosfile is generally the 3-word autoload sequence
 
 Starts at IP 0.
 stdio is used instead of typewriter and paper tape, redirection can be used for file.
 
-## sexadecimal:
+## Compilation
+
+You'll need a little endian computer, make and gcc.
+
+    make
+
+## License, Contributing, etc
+
+ISC License
+
+## Resources
+
+https://www.pearcey.org.au/blog/2020/snocom-and-cirrus/
+
+https://ses.library.usyd.edu.au/handle/2123/16005
+
+### sexadecimal:
 
 Not as fun as it sounds.
 
--- snocom.c
+++ snocom.c
@@ -60,6 +60,7 @@ int main(int argc, char** argv) {
         printf("ip: %#010x, instruction: %#010x\n", instructionpointer, memory[instructionpointer]);
 
         int instruction = decodeInstruction();
+        int address = decodeAddress();
 
         switch(instruction) {
             case 0: // stop
@@ -79,26 +80,45 @@ int main(int argc, char** argv) {
                     printf("INPUT: ACCUMULATOR = %#010x\n", accumulator);
                 }
                 break;
+
+            case 8: // output
+                putchar((char)address); // for now assume it's less than 8 bits.
+                break;
                 
             case 12: // store
-                int address = decodeAddress();
                 memory[address] = accumulator;
                 printf("STORE: MEMORY[%d] = %#010x\n", address, accumulator);
                 break;
 
-            case 1: // bring
+            case 13: // store and clear
+                memory[address] = accumulator;
+                accumulator = 0;
+                printf("STORE AND CLEAR: MEMORY[%d] = %#010x\n", address, accumulator);
+                break;
+
+            case 1: // bring (load)
+                accumulator = memory[address];
+                printf("BRING: ACCUMULATOR = %#010x\n", accumulator);
+                break;
+                
+            case 14: // add - TODO: fix this to use the actual machine arithmetic
+                accumulator = memory[address] + accumulator;
+                printf("ADD: ACCUMULATOR = %#010x\n", accumulator);
+                break;
+                
+            case 15: // subtract - TODO: fix this to use the actual machine arithmetic
+                accumulator = accumulator - memory[address];
+                printf("SUB: ACCUMULATOR = %#010x\n", accumulator);
+                break;
+
             case 2: // store address
             case 3: // return address
             case 5: // divide
             case 6: // multiply integers
             case 7: // multiply fractions
-            case 8: // output
             case 9: // extract
             case 10: // unconditional transfer
-            case 11: // conditional transfer
-            case 13: // store and clear
-            case 14: // add
-            case 15: // subtract
+            case 11: // conditional transfer - note this has a sub instruction where we will need to wait for user input
 
             default:
                 printf("Instruction: %d\n", instruction);
